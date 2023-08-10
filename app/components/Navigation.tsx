1138a1aeef1a4b850722f4bf1f6eaf83be06fc2a
Lucas Galvez <lgalvez@utec.edu.pe>
2023-08-10 00:18:30 -0500

    No tailwind and start with custom styles

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import styles from "./Navigation.module.css"
 
 const links = [
     {
@@ -13,9 +14,9 @@ const links = [
 
 const Navigation = () => {
     return (
-        <header>
+        <header className={styles.header}>
             <nav>
-                <ul>
+                <ul className={styles.navigation}>
                     {links.map(({ label, route }) => (
                         <li key={route}>
                             <Link href={route}>{label}</Link>
